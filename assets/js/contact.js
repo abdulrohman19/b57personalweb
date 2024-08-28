a1dd8a4e877c5a0861dbb700a9e426b0e537e830
abdul rohman <abdulrohmanunida@gmail.com>
2024-08-29 00:02:23 +0700

    Day 3 - Form Submission

---
+++ assets/js/contact.js
@@ -0,0 +1,5 @@
+const inputName = document.getElementById("name").value;
+const inputEmail = document.getElementById("email").value:
+const inputPhoneNumber = document.getElementById("phoneNumber").value;
+const inputSubject = document.getElementById("subject").value;
+const inputMessage = document.getElementById("message").value;
