d660fb67716330a8f72d95fd223f0dd42b9f4fee
abdul rohman <abdulrohmanunida@gmail.com>
2024-08-31 00:14:01 +0700

    Day 3 - update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,9 @@
 const inputName = document.getElementById("name").value;
-const inputEmail = document.getElementById("email").value:
+const inputEmail = document.getElementById("email").value;
 const inputPhoneNumber = document.getElementById("phoneNumber").value;
 const inputSubject = document.getElementById("subject").value;
 const inputMessage = document.getElementById("message").value;
+
+alert(
+  `Name: ${inputName}\nEmail: ${inputEmail}\nPhone Number: ${inputPhoneNumber}\nSubject: ${inputSubject}\nMessage: ${inputMessage}`
+);
