ef2b1300c0284b89e85f96d4efe45560974daaed
abdul rohman <abdulrohmanunida@gmail.com>
2024-08-31 23:59:30 +0700

    Day 3 - Connected to Contact js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,15 @@
-const inputName = document.getElementById("name").value;
-const inputEmail = document.getElementById("email").value;
-const inputPhoneNumber = document.getElementById("phoneNumber").value;
-const inputSubject = document.getElementById("subject").value;
-const inputMessage = document.getElementById("message").value;
+function sendEmail(event) {
+  event.preventDefault();
+   
+  const inputName = document.getElementById("name").value;
+  const inputEmail = document.getElementById("email").value;
+  const inputPhoneNumber = document.getElementById("phoneNumber").value;
+  const inputSubject = document.getElementById("subject").value;
+  const inputMessage = document.getElementById("message").value;
 
-alert(
-  `Name: ${inputName}\nEmail: ${inputEmail}\nPhone Number: ${inputPhoneNumber}\nSubject: ${inputSubject}\nMessage: ${inputMessage}`
-);
+  
+  alert(
+    `Name: ${inputName}\nEmail: ${inputEmail}\nPhone Number: ${inputPhoneNumber}\nSubject: ${inputSubject}\nMessage: ${inputMessage}`
+  );
+
+}
