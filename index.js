64029475db8e13dadeb5df2656fd073402dd239d
abdul rohman <abdulrohmanunida@gmail.com>
2024-09-16 06:17:46 +0700

    update

--- index.js
+++ index.js
@@ -3,27 +3,45 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
-app.set("view engine", "hbs")
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./views"));
 
 app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use(express.urlencoded({ extended: true }));
 
 // routing
-app.get("/", (request, response) => {
-    response.render("index");
-});
-app.get("/my-project", (request, response) => {
-    response.render("my-project");
-});
-app.get("/contact", (request, response) => {
-    response.render("contact");
-});
-app.get("/testimonial", (request, response) => {
-    response.render("testimonial");
-});
-app.get("/project-detail", (request, response) => {
-    response.render("project-detail");
-});
-app.listen(port, () =>{
-    console.log(`Server berjalan di port ${port}`);
+app.get("/", home);
+
+function home(request, response) {
+  response.render("index");
+}
+
+app.get("/my-project", myProject);
+
+function myProject(request, response) {
+  response.render("my-project");
+}
+
+// app.post("/my-project", myProject);
+// function myProject(request, response) {
+//   console.log("Sukses post form");
+// }
+
+app.get("/contact", contact);
+function contact(request, response) {
+  response.render("contact");
+}
+
+app.get("/testimonial", testimonial);
+function testimonial(request, response) {
+  response.render("testimonial");
+}
+
+app.get("/project-detail", projectDeatil);
+function projectDeatil(request, response) {
+  response.render("project-detail");
+}
+
+app.listen(port, () => {
+  console.log(`Server berjalan di port ${port}`);
 });
