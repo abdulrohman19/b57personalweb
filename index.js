a48725686ca1a8c9858067eb32a9d8382c2653e6
abdul rohman <abdulrohmanunida@gmail.com>
2024-09-15 15:38:04 +0700

    Day 11 - Used Hbs

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
 
+app.set("view engine", "hbs")
+app.set("views", path.join(__dirname, "./views"));
+
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+// routing
 app.get("/", (request, response) => {
-    response.send("Hello World!")
-})
-
+    response.render("index")
+});
+app.get("/about", (request, response) => {
+    response.send("About me")
+});
 app.listen(port, () =>{
     console.log(`Server berjalan di port ${port}`)
-})
+});
